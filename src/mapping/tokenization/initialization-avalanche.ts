5185230043e3b6eefba77143b014cf5b9ad23070
kustrun <domen.balantic1@gmail.com>
2022-05-24 19:22:12 +0200

    Remove redundant code, setup Nereus fork.

--- src/mapping/tokenization/initialization-avalanche.ts
+++ src/mapping/tokenization/initialization-avalanche.ts
@@ -1,16 +1,11 @@
 import { Initialized as ATokenInitialized } from '../../../generated/templates/AToken/AToken';
 import { Initialized as VTokenInitialized } from '../../../generated/templates/VariableDebtToken/VariableDebtToken';
 import { Initialized as STokenInitialized } from '../../../generated/templates/StableDebtToken/StableDebtToken';
-import { AaveIncentivesController as AaveIncentivesControllerTemplate } from '../../../generated/templates';
-import { AaveIncentivesController as AaveIncentivesControllerC } from '../../../generated/templates/AaveIncentivesController/AaveIncentivesController';
 import {
   ContractToPoolMapping,
-  IncentivesController,
   MapAssetPool,
 } from '../../../generated/schema';
-import { Address, log } from '@graphprotocol/graph-ts';
-import { IERC20Detailed } from '../../../generated/templates/AToken/IERC20Detailed';
-import { zeroAddress } from '../../utils/converters';
+import {Address} from '@graphprotocol/graph-ts';
 export {
   handleATokenBurn,
   handleATokenMint,
@@ -29,33 +24,6 @@ function createIncentivesController(
   underlyingAsset: Address,
   pool: Address
 ): void {
-  if (incentivesController == zeroAddress()) {
-    log.warning('Incentives controller is 0x0 for asset: {} | underlyingasset: {} | pool: {}', [
-      asset.toHexString(),
-      underlyingAsset.toHexString(),
-      pool.toHexString(),
-    ]);
-    return;
-  }
-  let iController = IncentivesController.load(incentivesController.toHexString());
-  if (!iController) {
-    iController = new IncentivesController(incentivesController.toHexString());
-    // get incentive reward info
-    let AaveIncentivesControllerContract = AaveIncentivesControllerC.bind(incentivesController);
-    let rewardToken = AaveIncentivesControllerContract.REWARD_TOKEN();
-    let precision = AaveIncentivesControllerContract.PRECISION();
-    let emissionEndTimestamp = AaveIncentivesControllerContract.DISTRIBUTION_END();
-    let IERC20DetailedContract = IERC20Detailed.bind(rewardToken);
-    let rewardTokenDecimals = IERC20DetailedContract.decimals();
-    let rewardTokenSymbol = IERC20DetailedContract.symbol();
-    iController.rewardToken = rewardToken;
-    iController.rewardTokenDecimals = rewardTokenDecimals;
-    iController.rewardTokenSymbol = rewardTokenSymbol;
-    iController.precision = precision;
-    iController.emissionEndTimestamp = emissionEndTimestamp.toI32();
-    iController.save();
-    AaveIncentivesControllerTemplate.create(incentivesController);
-  }
   let poolAddressProvider = ContractToPoolMapping.load(pool.toHexString());
   if (poolAddressProvider != null) {
     // save asset pool mapping
