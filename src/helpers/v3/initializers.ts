245ef66bbd5c84cfb51091e1fe0032dd5d95371f
aschmidt20 <andrew@aave.com>
2023-03-09 00:28:35 -0600

    feat: gho discount initializer

--- src/helpers/v3/initializers.ts
+++ src/helpers/v3/initializers.ts
@@ -1,5 +1,5 @@
 import { Bytes, Address, ethereum, log } from '@graphprotocol/graph-ts';
-import { Facilitator } from '../../../generated/schema';
+import { Facilitator, GhoDiscount } from '../../../generated/schema';
 import {
   SubToken,
   PriceOracle,
@@ -24,6 +24,7 @@ import {
   zeroBI,
 } from '../../utils/converters';
 import { getReserveId, getUserReserveId } from '../../utils/id-generation';
+import { GhoDiscountRateStrategy } from '../../../generated/GhoVariableDebtToken/GhoDiscountRateStrategy';
 
 export function getProtocol(): Protocol {
   let protocolId = '1';
@@ -361,4 +362,39 @@ export function getOrInitGhoFlashMinter(contractAddress: Address): GhoFlashMinte
     ghoFlashMinter.save();
   }
   return ghoFlashMinter as GhoFlashMinter;
+}
+
+export function updateOrInitGhoDiscount(contractAddress: Address): GhoDiscount {
+  let ghoDiscount = GhoDiscount.load('1');
+  if (!ghoDiscount) {
+    ghoDiscount = new GhoDiscount('1');
+  }
+  const discountStrategyContract = GhoDiscountRateStrategy.bind(contractAddress);
+  ghoDiscount.discountToken = zeroAddress();
+  const discountRate = discountStrategyContract.try_DISCOUNT_RATE();
+  if (!discountRate.reverted) {
+    ghoDiscount.discountRate = discountRate.value;
+  } else {
+    ghoDiscount.discountRate = zeroBI();
+  }
+  const ghoDiscountedPerDiscountToken = discountStrategyContract.try_GHO_DISCOUNTED_PER_DISCOUNT_TOKEN();
+  if (!ghoDiscountedPerDiscountToken.reverted) {
+    ghoDiscount.ghoDiscountedPerDiscountToken = ghoDiscountedPerDiscountToken.value;
+  } else {
+    ghoDiscount.ghoDiscountedPerDiscountToken = zeroBI();
+  }
+  const minDebtTokenBalance = discountStrategyContract.try_MIN_DEBT_TOKEN_BALANCE();
+  if (!minDebtTokenBalance.reverted) {
+    ghoDiscount.minDebtTokenBalance = minDebtTokenBalance.value;
+  } else {
+    ghoDiscount.minDebtTokenBalance = zeroBI();
+  }
+  const minDiscountTokenBalance = discountStrategyContract.try_MIN_DISCOUNT_TOKEN_BALANCE();
+  if (!minDiscountTokenBalance.reverted) {
+    ghoDiscount.minDiscountTokenBalance = minDiscountTokenBalance.value;
+  } else {
+    ghoDiscount.minDiscountTokenBalance = zeroBI();
+  }
+  ghoDiscount.save();
+  return ghoDiscount;
 }
